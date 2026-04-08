189e5ff52bea967693d71e7580771ca1312b8e49
Arggonaut <and123zhe@gmail.com>
2026-04-08 16:47:49 -0400

    recieveBlock now adds the new block to the chain

--- src/blockchain.py
+++ src/blockchain.py
@@ -36,50 +36,49 @@ class BlockChainClient:
         The goal here is to verify that the block that this node recieved adheres to 
         the rules of the blockchain (i.e correctly calcukated nonce, right reward, etc.)
         '''
-        #validate that hash starts with (difficulty) zeros
-        #using the nonse gets the hash
-        #block.previous is the last in the chain
-        #index is what would be next in the chain
-        #transactions is one more than what was in the previous block
-        '''todo: validate transactions'''
-        #timestamp is after the timestamp of the previous block
-
+        
+        #declare previousBlock and set it to the last block in the chain
         previousBlock = self.chain[-1]
         
+        #check the hash has the correct number of leading zeros
         for i in range(difficulty):
             if block.hash[i] != 0:
                 return -1
             
+        #check that the nonse gets the hash, previousBlock is the last block in the chain, 
+        #block.index is the next index, and block.timestamp comes after previousblock.timestamp
         if (block.hash != block.calculateHash()) or (previousBlock.hash != block.previousHash) \
             or (previousBlock.index != (block.index - 1)) or (previousBlock.timestamp >= block.timestamp):
             return -1
-
         
 
-            
-        
-        pass
+        #add the block to the chain
+        self.chain.append(block)
+
 
     def mine(self):
         if len(self.mempool) >= 10:
             print(f"Starting mining process on {self.name}")
 
-            #create a new block, look at the first (difficulty) characters, 
-            # if they aren't all zeros generate a new nonce and check again
-            # transmit it.
+            #declare previousBlock and set it to the last block in the chain
             previousBlock = self.chain[-1]
 
             newBlock = Block(previousBlock.index + 1, self.mempool, previousBlock.hash) #create a new block
             validHash = False #declare a validHash flag and set it to false
-            while validHash == False: #loop until we get a valid hash
-                validHash = True #set validHash to true for now
-                for i in range(difficulty): #look at the first (difficulty) characters in the hash
-                    if newBlock.hash[i] != 0: #if any of them aren't 0
-                        validHash = False #since hash is invalid, set validHash to False
-                        newBlock.nonce = random.randint(0,9223372036854775807) #generate a new nonse from 0 to int limit
-                        newBlock.hash = newBlock.calculateHash() #recalculate hash
-                        break #break since hash has changed
+            
+            #try out nonces until it gives a hash with (difficulty) leading zeros
+            while validHash == False:           
+                validHash = True               
+                for i in range(difficulty):    
+                    if newBlock.hash[i] != 0:
+                        validHash = False
+                        newBlock.nonce = random.randint(0,9223372036854775807)
+                        newBlock.hash = newBlock.calculateHash() 
+                        break
                 
-
+            #have each peer validate and add the block to their chain
             for i in self.peers:
                 i.recieveBlock(newBlock)
+
+            #validate and add the block to the chain
+            self.recieveBlock(newBlock)
